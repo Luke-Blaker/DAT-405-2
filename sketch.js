49ea859e9291e1f71d30d1109b497c81eb7ce8d2
BuildTools <unconfigured@null.spigotmc.org>
2017-12-27 16:01:20 +0000

    Began integration of openweathermap api.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
-function setup(){
-createCanvas(1280,720);
-background(255,0,0);
-}
+let apiKey = "5731e7f2d05169da768d85ade6745bc5";
+let weather;
 
 function loading(){
   let UserInput = document.getElementById('city');
   console.log(UserInput.value);
+  let url = "http://api.openweathermap.org/data/2.5/weather?q="+UserInput.value+"&units=metric&appid="+apiKey;
+  weather = loadJSON(url);
+  call()
+}
+
+function setup(){
+
+  createCanvas(1280,720);
+  background(255,0,0);
+}
+function call(){
+  console.log(weather);
 }
