b867088a9673bf63a6d59b4a8804ab172cc36a29
BuildTools <unconfigured@null.spigotmc.org>
2017-12-29 13:22:11 +0000

    Little progress

--- sketch.js
+++ sketch.js
@@ -1,19 +1,28 @@
-let apiKey = "5731e7f2d05169da768d85ade6745bc5";
+//apiKey = 5731e7f2d05169da768d85ade6745bc5
 let weather;
-
-function loading(){
-  let UserInput = document.getElementById('city');
-  console.log(UserInput.value);
-  let url = "http://api.openweathermap.org/data/2.5/weather?q="+UserInput.value+"&units=metric&appid="+apiKey;
-  weather = loadJSON(url);
-  call()
-}
+let UserInput;
 
 function setup(){
-
   createCanvas(1280,720);
   background(255,0,0);
 }
+function loading(){
+  UserInput = document.getElementById('city');
+  console.log(UserInput.value);
+  //weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q="+UserInput.value+"&units=metric&appid=5731e7f2d05169da768d85ade6745bc5");
+  weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Exeter&units=metric&appid=5731e7f2d05169da768d85ade6745bc5");
+  call()
+}
 function call(){
-  console.log(weather);
+  console.log(weather.value);
+  console.log("Location: " + UserInput.value);
+  console.log("Temperature: " + weather.main.temp.value + "°C");
+  console.log("Temperature (min): " + weather.main.temp_min + "°C");
+  console.log("Temperature (max): " + weather.main.temp_max + "°C");
+  console.log("Humidity: " + weather.main.humidity);
+  console.log("Pressure: " + weather.main.pressure);
+}
+
+function graphics(){
+
 }
