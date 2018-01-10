0796a261422ac716237efbd879898bacd8ac0b39
BuildTools <unconfigured@null.spigotmc.org>
2018-01-10 00:00:15 +0000

    Slight reworking of previos commit.

--- sketch.js
+++ sketch.js
@@ -8,18 +8,12 @@ let height = 720;
 function preload(){
   createCanvas(width,height);
   background(155,70,155);
-  weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Exeter&units=metric&appid=5731e7f2d05169da768d85ade6745bc5")
-  //weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q="+UserInput+"&units=metric&appid=5731e7f2d05169da768d85ade6745bc5");
-
+  weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Plymouth&units=metric&appid=5731e7f2d05169da768d85ade6745bc5")
 }
 
 function setup(){
-  UserInput = document.getElementById('city')
-  console.log(UserInput.value);
-  //weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q="+UserInput.value+"&units=metric&appid=5731e7f2d05169da768d85ade6745bc5");
-  //weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Exeter&units=metric&appid=5731e7f2d05169da768d85ade6745bc5")
-  console.log(weather.value);
-  console.log("Location: " + UserInput.value);
+    console.log(weather.value);
+  console.log("Location: Plymouth" );
   console.log("Temperature: " + weather.main.temp + "°C");
   console.log("Temperature (min): " + weather.main.temp_min + "°C");
   console.log("Temperature (max): " + weather.main.temp_max + "°C");
