ef7bb4002bc0ff84fbcb1b5d5eefd1f559ef4671
BuildTools <unconfigured@null.spigotmc.org>
2018-01-11 18:09:06 +0000

    Added display for weather on canvas/

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ let width = 1280;
 let height = 720;
 let SRtimeFormat;
 let SStimeFormat;
-let canvas;
-let ctx;
+
+
 let CurrentTime;
 let ImgDay = new Image();
 let ImgNight = new Image();
@@ -17,7 +17,6 @@ let ImgNight = new Image();
 
 function preload(){
   createCanvas(width,height);
-  background(155,70,155);
   ImgDay = loadImage("https://78.media.tumblr.com/2c083396ce86fa24a8738566e54df66d/tumblr_p2coixXeWp1wcwkz8o1_1280.jpg");
   ImgNight = loadImage("https://78.media.tumblr.com/9f9f4ed7aec4be7db8f2b79c2480606c/tumblr_p2coixXeWp1wcwkz8o2_1280.jpg");
   weather = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Plymouth&units=metric&appid=5731e7f2d05169da768d85ade6745bc5")
@@ -57,19 +56,44 @@ function SunTimeConversion(){
   console.log("Sunset (hh/mm) : " + SStimeFormat);
 }
 
-  function DayNightBackground(){
-    CurrentTime = new Date().toLocaleTimeString();
-    console.log("System time: " +CurrentTime);
-    if (CurrentTime >= SRtimeFormat){
-      image(ImgDay,0,0);
-    }
-    if (CurrentTime >= SStimeFormat){
-      image(ImgNight,0,0);
-    }
-  }
+function InfoPane(){
+  noStroke();
+  fill(0,20,50, 1);
+  rect(0,0,400,719,0,15,15,0);
+  InfoPaneText();
+}
+
+function InfoPaneText(){
+  noStroke();
+  textSize(50);
+  fill(255,255,255,10);
+  text("Weather  Results",40,60);
+  text("Location: Plymouth",800,60);
+  textSize(20)
+  text("Weather at a glance: " + weather.main,60, 120);
+  text("Sunrise (hh/mm) : " + SRtimeFormat,60,180);
+  text("Sunset (hh/mm) : " + SStimeFormat,60,240);
+  //text("Sunrise: " + weather.sys.sunrise + " (unix)");
+  //text("Sunset: " + weather.sys.sunset + " (unix)");
+  text("Temperature: " + weather.main.temp + "°C",60,300);
+  //text("Temperature (min): " + weather.main.temp_min + "°C",60,180);
+  //text("Temperature (max): " + weather.main.temp_max + "°C",60,240);
+  text("Humidity: " + weather.main.humidity,60,360);
+  text("Pressure: " + weather.main.pressure,60,420);
+  text("Cloudiness: "+ weather.clouds.all + "%",60,480) ;
+  text("Rainfall: " + weather.rain,60,540);
+  text("Snowfall: " + weather.snow,60,600);
+  text("Windspeed: "+ weather.wind.speed*2.3 + " Mph",60,660);
+
+}
+
 
 function sun(){
    'pow(abs(sin(t*2))*0.6,sin(t*2))*0.6'
+}
+
+function moon(){
+
 }
 
 function rain(){
@@ -88,13 +112,20 @@ function clouds(){
 
 }
 
-function InfoPane(){
-  fill(51,15);
-  noStroke();
-  rect(0,0,320,719,0,15,15,0);
 
+function DayNightBackground(){
+  CurrentTime = new Date().toLocaleTimeString();
+  console.log("System time: " +CurrentTime);
+  if (CurrentTime >= SRtimeFormat){
+    image(ImgDay,0,0);
+  }
+  if (CurrentTime >= SStimeFormat){
+    image(ImgNight,0,0);
+  }
 }
 
 function draw(){
   InfoPane();
+
+
 }
