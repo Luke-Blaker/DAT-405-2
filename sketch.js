0206d743ea9ce56f594af5821225f1e5850490ac
BuildTools <unconfigured@null.spigotmc.org>
2018-01-12 00:37:32 +0000

    Final commit for 2nd DAT405 submission. Will continue to work on...

--- sketch.js
+++ sketch.js
@@ -3,11 +3,16 @@
 
 //apiKey = 5731e7f2d05169da768d85ade6745bc5
 
-let weather;
+//Setting up all necessary global variables
+let result;
 let width = 1280;
 let height = 720;
+
+//These variables are for the unix timestamps,
+//once reformatted to a 24hour version
 let SRtimeFormat;
 let SStimeFormat;
+
 let currentWeather;
 let temp;
 let humidity;
@@ -16,32 +21,29 @@ let cloudiness;
 let rainfall;
 let snowfall;
 let windspeed;
-
-
 let CurrentTime;
-let ImgDay = new Image();
-let ImgNight = new Image();
 
-
+//This function loads the OpenWeatherMap JSON and the images required
+//which are linked from my course tumblr page; tagged 'weather_visualiser'
 function preload(){
   createCanvas(width,height);
   ImgDay = loadImage("https://78.media.tumblr.com/2c083396ce86fa24a8738566e54df66d/tumblr_p2coixXeWp1wcwkz8o1_1280.jpg");
   ImgNight = loadImage("https://78.media.tumblr.com/9f9f4ed7aec4be7db8f2b79c2480606c/tumblr_p2coixXeWp1wcwkz8o2_1280.jpg");
   Cloud = loadImage("https://78.media.tumblr.com/e07d7320cb4303ebac0c0dbe1fec88d1/tumblr_p2etllqQHz1wcwkz8o1_540.png");
   result = loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Plymouth&units=metric&appid=5731e7f2d05169da768d85ade6745bc5")
+}                                          //Location can be edited ^ h e r e ^ .
 
-}
-
+//Here, certain results are assigned to the third portion of global
+//variables. Then they are printed to the console
 function setup(){
-
-  currentWeather = result.weather[0].main;
+  currentWeather = result.weather[0].main; //reads weather id
   temp = result.main.temp;
   humidity = result.main.humidity;
   pressure = result.main.pressure;
   cloudiness = result.clouds.all;
-  rainfall = result.rain;
-  snowfall =  result.snow;
-  windspeed = result.wind.speed*2.3;
+  rainfall = result.rain; //These two are currently not working
+  snowfall =  result.snow; // ~~
+  windspeed = result.wind.speed*2.3; //Converted from Metric Mps to Imperial Mph
   console.log(result);
   console.log("Location: Plymouth" );
   console.log("Weather at a glance: " + currentWeather);
@@ -56,26 +58,28 @@ function setup(){
   console.log("Rainfall: " + rainfall);
   console.log("Snowfall: " + snowfall);
   console.log("Windspeed: "+ windspeed+ " Mph");
-  SunTimeConversion();
-  DayNightBackground();
-  console.log(currentWeather);
-}
+  SunTimeConversion(); //calls the function to convert the unix timestamps
+  DayNightBackground(); //calls the function which changes the background based on search query's sunrise and sunset times
+  }
 
+//This function converts the unix timestamps into a 24 hour version
 function SunTimeConversion(){
-  let SRtime = new Date(result.sys.sunrise*1000);
-  let SRtimeNewH = "0" + SRtime.getHours();
-  let SRtimeNewM = "0" + SRtime.getMinutes();
-  SRtimeFormat = SRtimeNewH + ":" + SRtimeNewM.substr(-2);
-  //SRtimeFormat = SRtimeFormat.slice(1,6)
-  let SStime = new Date(result.sys.sunset*1000);
+  let SRtime = new Date(result.sys.sunrise*1000); //Converted from milliseconds to seconds
+  let SRtimeNewH = "0" + SRtime.getHours(); //value then used to get the hour of sunrise, assigned to new variable
+  let SRtimeNewM = "0" + SRtime.getMinutes(); //same value used to get the minute of sunrise, assigned to new variable
+  SRtimeFormat = SRtimeNewH + ":" + SRtimeNewM.substr(-2); //These variables are then added together, and assigned to a new variable for the time of sunrise
+  SRtimeFormat = SRtimeFormat.slice(0,6) //This variable is then sliced to ensure only 4 digits.
+  let SStime = new Date(result.sys.sunset*1000); //The next lines perform the same actions as previously, but for the time of sunset
   let SStimeNewH = "0" + SStime.getHours();
   let SStimeNewM = "0" + SStime.getMinutes();
   SStimeFormat = SStimeNewH + ":" + SStimeNewM.substr(-2);
   SStimeFormat = SStimeFormat.slice(1,6)
-  console.log("Sunrise (hh/mm) : " + SRtimeFormat);
+  console.log("Sunrise (hh/mm) : " + SRtimeFormat); //Times are printed in the console
   console.log("Sunset (hh/mm) : " + SStimeFormat);
 }
 
+//This function creates medium grey rectangle with the
+//right hand vertices rounded at 15 degrees
 function InfoPane(){
   noStroke();
   fill(0,20,50, 1);
@@ -83,6 +87,7 @@ function InfoPane(){
   InfoPaneText();
 }
 
+//With that, this function displays a formatted version of the weather results
 function InfoPaneText(){
   noStroke();
   textSize(50);
@@ -93,29 +98,17 @@ function InfoPaneText(){
   text("Weather at a glance: " + currentWeather,60, 120);
   text("Sunrise (hh/mm) : " + SRtimeFormat,60,180);
   text("Sunset (hh/mm) : " + SStimeFormat,60,240);
-  //text("Sunrise: " + weather.sys.sunrise + " (unix)");
-  //text("Sunset: " + weather.sys.sunset + " (unix)");
   text("Temperature: " + temp + "°C",60,300);
-  //text("Temperature (min): " + weather.main.temp_min + "°C",60,180);
-  //text("Temperature (max): " + weather.main.temp_max + "°C",60,240);
-  text("Humidity: " + humidity,60,360);
+  text("Humidity: " + humidity + "%",60,360);
   text("Pressure: " + pressure,60,420);
   text("Cloudiness: "+ cloudiness + "%",60,480) ;
   text("Rainfall: " + rainfall,60,540);
   text("Snowfall: " + snowfall,60,600);
   text("Windspeed: "+ windspeed + " Mph",60,660);
-
 }
 
-
-function sun(){
-   'pow(abs(sin(t*2))*0.6,sin(t*2))*0.6'
-}
-
-function moon(){
-
-}
-
+//This is the first of the visualisations.
+//This function fills the canvas randomly with a blue '|' to simulate rain
 function rain(){
     if (currentWeather == "Rain"){
       let drop = "|";
@@ -130,11 +123,7 @@ function rain(){
     }
   }
 
-
-function wind(){
-
-}
-
+//This function does the same but instead displays a white '*'
 function snow(){
   if (currentWeather == "Snow"){
     let flake = "*";
@@ -149,23 +138,41 @@ function snow(){
   }
 }
 
+//More of a filler visualisation, but this function
+//displays a cloud in the centre of the screen
 function clouds(){
   if (currentWeather == "Clouds"){
     image(Cloud,width/2,100)}
-  }
+}
 
+//The following are weathers and conditions that I would like to add soon,
+//There are 75 possible weathers provided by OpenWeatherMap
+function sun(){
 
+}
+
+function moon(){
+
+}
+
+function wind(){
+
+}
+
+//This function changes the background from day to night
+//based on the sunrise and set times and the system time.
 function DayNightBackground(){
   CurrentTime = new Date().toLocaleTimeString();
   console.log("System time: " +CurrentTime);
   if (CurrentTime >= SRtimeFormat){
     image(ImgDay,0,0);
   }
-  if (CurrentTime >= SStimeFormat){
+  if (CurrentTime >= SStimeFormat || CurrentTime < SRtimeFormat){
     image(ImgNight,0,0);
   }
 }
 
+//Draw function to call all uncalled functions
 function draw(){
   InfoPane();
   clouds();
